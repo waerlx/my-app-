2eb4c3f38922e70f3c5d36c7dfdb48cba061eb38
Adelina <adelinaakylbekova565@gmail.com>
2024-04-15 15:43:04 +0600

    add btn for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,18 @@
+import React, { useContext } from "react"
+import { Context } from ".."
+import firebase from 'firebase/compat/app'
 function Login(){
+    const {auth} =  useContext(Context)
+    const login = async()=>{
+        const provider = new firebase.auth.GoogleAuthProvider()
+        const {user} = await auth.signInWithpopup(provider)
+        console.log(user)
+    }
     return(
         <>
-        login
+        <button>Enter with Google</button>
         </>
+        
     )
 }
 export default Login
